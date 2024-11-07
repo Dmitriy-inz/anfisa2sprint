dc8eb9f31588b53baa9469506ac144c52078cc9d
Дмитрий Иноземцев <Dmitriy-inz@yandex.ru>
2024-11-07 21:46:50 +0300

    Error correction #3

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -4,23 +4,79 @@ from core.models import PublishedModel
 
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
-    output_order = models.PositiveSmallIntegerField(default=100)
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано'
+    )
+    title = models.CharField(max_length=256, verbose_name='Название')
+    slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
 
 
 class Topping(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано'
+    )
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        max_length=64,
+        unique=True,
+        verbose_name='Слаг'
+    )
+
+    class Meta:
+        verbose_name = 'топпинг'
+        verbose_name_plural = 'Топпинги'
+
+    def __str__(self):
+        return self.title
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано'
+    )
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Название'
+    )
+
+    class Meta:
+        verbose_name = 'обертка'
+        verbose_name_plural = 'Обертки'
+
+    def __str__(self):
+        return self.title
 
 
 class IceCream(PublishedModel):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано'
+    )
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Название'
+    )
+
+    description = models.TextField(
+        verbose_name='Описание'
+    )
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
@@ -32,7 +88,19 @@ class IceCream(PublishedModel):
         Category,
         on_delete=models.CASCADE,
         related_name='ice_creams',
+        verbose_name='категория'
     )
-    toppings = models.ManyToManyField(Topping)
-    is_on_main = models.BooleanField(default=False)
+    toppings = models.ManyToManyField(
+        Topping,
+        verbose_name='топпинги')
+    is_on_main = models.BooleanField(
+        default=False,
+        verbose_name='На главную'
+    )
 
+    class Meta:
+        verbose_name = 'мороженое'
+        verbose_name_plural = 'Мороженое'
+
+    def __str__(self):
+        return self.title
